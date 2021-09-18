4a7d5258ba4a7f44feab135b6b01774a0c0a99c3
w40141 <w.40141@gmail.com>
2021-09-18 23:28:42 +0900

    input file chekcer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use clap::{Clap};
+use clap::Clap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 #[derive(Clap, Debug)]
 #[clap(
@@ -9,21 +11,34 @@ use clap::{Clap};
 )]
 
 struct Opts {
-    /// Sets the level of verbosity
+    // Sets the level of verbosity
     #[clap(short, long)]
     verbose: bool,
 
-    /// Formulas written in RPN
+    // Formulas written in RPN
     #[clap(name = "FILE")]
     formula_file: Option<String>,
 }
 
 pub fn main() {
     let opts = Opts::parse();
-    match opts.formula_file {
-        Some(file) => println!("File specified: {}", file),
-        None => println!("No file specified."),
-    }
 
-    println!("Is verbosity specified?: {}", opts.verbose);
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+
+        for line in reader.lines() {
+            let line = line.unwrap();
+            println!("{}", line);
+        }
+    } else {
+        // ファイルを指定しなかった場合
+        println!("No file is specified");
+    }
+    // match opts.formula_file {
+    //     Some(file) => println!("File specified: {}", file),
+    //     None => println!("No file specified."),
+    // }
+
+    // println!("Is verbosity specified?: {}", opts.verbose);
 }
