eaaa2b1065d18c128ee74f4aadcd57aef22a41b0
w40141 <w.40141@gmail.com>
2021-09-18 23:13:04 +0900

    make arg command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Clap};
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My PRN program",
+    version = "1.0.0",
+    author = "Daisuke Oku",
+    about = "Super awesome sample RPN caluculator"
+)]
+
+struct Opts {
+    /// Sets the level of verbosity
+    #[clap(short, long)]
+    verbose: bool,
+
+    /// Formulas written in RPN
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
+
+pub fn main() {
+    let opts = Opts::parse();
+    match opts.formula_file {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified."),
+    }
+
+    println!("Is verbosity specified?: {}", opts.verbose);
 }
