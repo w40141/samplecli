cc065ee81da7686fe1cc66de170228699bcf2bae
w40141 <w.40141@gmail.com>
2021-09-19 12:19:26 +0900

    add rpn function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Clap;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{stdin, BufRead, BufReader};
 
 #[derive(Clap, Debug)]
 #[clap(
@@ -20,6 +20,50 @@ struct Opts {
     formula_file: Option<String>,
 }
 
+struct RpnCalculator(bool);
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    pub fn eval(&self, formula: &str) -> i32 {
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.eval_inner(&mut tokens)
+    }
+
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+        let mut stack = Vec::new();
+
+        while let Some(token) = tokens.pop() {
+            if let Ok(x) = token.parse::<i32>() {
+                stack.push(x);
+            } else {
+                let y = stack.pop().expect("invalid syntax");
+                let x = stack.pop().expect("invalid syntax");
+                let res = match token {
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => panic!("invalid token"),
+                };
+                stack.push(res);
+            }
+
+            if self.0 {
+                println!("{:?} {:?}", tokens, stack);
+            }
+        }
+
+        if stack.len() == 1 {
+            stack[0]
+        } else {
+            panic!("invalid syntax")
+        }
+    }
+}
+
 pub fn main() {
     let opts = Opts::parse();
 
@@ -27,18 +71,21 @@ pub fn main() {
         let f = File::open(path).unwrap();
         let reader = BufReader::new(f);
 
-        for line in reader.lines() {
-            let line = line.unwrap();
-            println!("{}", line);
-        }
+        run(reader, opts.verbose)
     } else {
-        // ファイルを指定しなかった場合
-        println!("No file is specified");
-    }
-    // match opts.formula_file {
-    //     Some(file) => println!("File specified: {}", file),
-    //     None => println!("No file specified."),
-    // }
+        let stdin = stdin();
+        let reader = stdin.lock();
 
-    // println!("Is verbosity specified?: {}", opts.verbose);
+        run(reader, opts.verbose);
+    }
+}
+
+fn run<R: BufRead>(reader: R, verbose: bool) {
+    let calc = RpnCalculator::new(verbose);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let answer = calc.eval(&line);
+        println!("{}", answer);
+    }
 }
